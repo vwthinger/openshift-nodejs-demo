3fd734afa0c7052e84466029ab4cfd065925df2a
vwthinger <81942392+vwthinger@users.noreply.github.com>
2021-08-14 17:09:47 -0600

    Added variable

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const server = http.createServer((req, res) => {
     res.writeHead(200, {
         "Context-type": "text/html",
     });
-    res.end("<h1>Hello World from OpenShift NodeJS Application!!!!</h1>");
+    res.end(`<h1>Hello World from OpenShift NodeJS Application in Pod Name ${process.env.MY_POD_NAME}</h1>`);
 });
 
 server.listen(8080, () => {
-    console.log("Listening to requests on Port 8000");
+    console.log("Listening to requests on Port 8080");
 });
