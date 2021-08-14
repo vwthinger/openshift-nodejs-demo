9689fc4f3a35bc87d2fc915f4bd5d90a1e183c75
vwthinger <81942392+vwthinger@users.noreply.github.com>
2021-08-14 16:24:46 -0600

    Updates

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const server = http.createServer((req, res) => {
     res.writeHead(200, {
         "Context-type": "text/html",
     });
-    res.end("<h1>Hello World from OpenShift NodeJS App</h1>");
+    res.end("<h1>Hello World from OpenShift NodeJS Application!</h1>");
 });
 
 server.listen(8080, () => {
