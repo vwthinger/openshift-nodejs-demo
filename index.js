e27b7fcda0c7fd3c20b01058f55d2628c74c788c
vwthinger <81942392+vwthinger@users.noreply.github.com>
2021-08-14 15:54:08 -0600

    New Files
    New Files

---
+++ index.js
@@ -0,0 +1,12 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, {
+        "Context-type": "text/html",
+    });
+    res.end("<h1>Hello World from OpenShift NodeJS App</h1>");
+});
+
+server.listen(8080, () => {
+    console.log("Listening to requests on Port 8000");
+});
